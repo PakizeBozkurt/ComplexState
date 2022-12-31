df76a414a110ea399183636f81c3666fa46ae8ee
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-31 18:51:11 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 import "./index.css";
 
 function App() {
-  const [fullName, setFullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
-    lName: ""
+    lName: "",
+    email: ""
   });
 
   function handleChange(event) {
-    const { value, name } = event.target;
+    const { name, value } = event.target;
 
-    setFullName((prevValue) => {
+    setContact((prevValue) => {
       if (name === "fName") {
         return {
           fName: value,
-          lName: prevValue.lName
+          lName: prevValue.lName,
+          email: prevValue.email
         };
       } else if (name === "lName") {
         return {
           fName: prevValue.fName,
-          lname: value
+          lname: value,
+          email: prevValue.email
+      };
+    }
+        else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lname: prevValue.lName,
+          email: value
         };
       }
     });
@@ -28,20 +38,27 @@ function App() {
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form>
         <input
           name="fName"
           onChange={handleChange}
           placeholder="First Name"
-          value={fullName.fName}
+          value={contact.fName}
         />
         <input
           name="lName"
           onChange={handleChange}
           placeholder="Last Name"
-          value={fullName.lName}
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          onChange={handleChange}
+          placeholder="Email"
+          value={contact.email}
         />
         <button>Submit</button>
       </form>
